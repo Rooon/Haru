5e62bea41fe701715794de67cc1c42f0951844d0
Rooni <Crispbaguette@gmail.com>
2016-06-10 02:17:25 +0200

    Dud

---
+++ Haru/FormHaru.xaml.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Haru
+{
+    public static class WindowsServices
+    {
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+
+    public partial class FormHaru : Window
+    {
+        public FormHaru()
+        {
+            InitializeComponent();
+            
+            Loaded += FormHaru_Loaded;
+        }
+
+        private void FormHaru_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Width /= 2;
+            this.Height /= 2;
+            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Top = System.Windows.SystemParameters.PrimaryScreenHeight - this.Height;
+            var hwnd = new WindowInteropHelper(this).Handle;
+            WindowsServices.SetWindowExTransparent(hwnd);
+        }
+
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id,
+            [In] uint fsModifiers,
+            [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        private HwndSource _source;
+        private const int HOTKEY_ID = 9001;
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            var helper = new WindowInteropHelper(this);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            RegisterHotKey();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            _source = null;
+            UnregisterHotKey();
+            base.OnClosed(e);
+        }
+
+        private void RegisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            const uint VK_F10 = 0x79;
+            const uint MOD_CTRL = 0x0005;
+            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, 0x0006, 0x34))
+            {
+                // handle error
+            }
+        }
+
+        private void UnregisterHotKey()
+        {
+            var helper = new WindowInteropHelper(this);
+            UnregisterHotKey(helper.Handle, HOTKEY_ID);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            new Snipper().ShowDialog();
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
---
+++ Haru/Snipper.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using System.IO;
+
+namespace Haru
+{
+
+    public partial class Snipper : Window
+    {
+        private System.Windows.Point p1;
+        private System.Windows.Point p2;
+
+        private BitmapSource screenshot;
+
+        public Snipper()
+        {
+            InitializeComponent();
+            ImageBrush ib = new ImageBrush();
+            screenshot = CopyScreen();
+            ib.ImageSource = screenshot;
+            snipcanvas.Background = ib;
+            MouseDown += Snipper_MouseDown;
+            MouseUp += Snipper_MouseUp;
+            MouseMove += Snipper_MouseMove;
+        }
+
+        private void Snipper_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            this.innerRect.Rect = new Rect(p1, e.GetPosition(this));
+        }
+
+        private void Snipper_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            p1 = e.GetPosition(this);
+            //throw new NotImplementedException();
+        }
+
+        private void Snipper_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            p2 = e.GetPosition(this);
+            System.Windows.Clipboard.SetImage(new CroppedBitmap(screenshot, new Int32Rect((int)p1.X, (int)p1.Y, (int)Math.Abs(p2.X - p1.X), (int)Math.Abs(p2.Y - p1.Y))));
+            DialogResult = true;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            Left = Screen.AllScreens.Min(screen => screen.Bounds.X);
+            Top = Screen.AllScreens.Min(screen => screen.Bounds.Y);
+            Width = Screen.AllScreens.Max(screen => screen.Bounds.X + screen.Bounds.Width);
+            Height = Screen.AllScreens.Max(screen => screen.Bounds.Y + screen.Bounds.Height);
+            outerRect.Rect = new Rect(0, 0, Width, Height);
+            innerRect.Rect = new Rect(Width / 4, Height / 4, Width/2, Height/2);
+        }
+
+        private static BitmapSource CopyScreen()
+        {
+            var left = Screen.AllScreens.Min(screen => screen.Bounds.X);
+            var top = Screen.AllScreens.Min(screen => screen.Bounds.Y);
+            var right = Screen.AllScreens.Max(screen => screen.Bounds.X + screen.Bounds.Width);
+            var bottom = Screen.AllScreens.Max(screen => screen.Bounds.Y + screen.Bounds.Height);
+            var width = right - left;
+            var height = bottom - top;
+
+            using (var screenBmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+            {
+                using (var bmpGraphics = Graphics.FromImage(screenBmp))
+                {
+                    bmpGraphics.CopyFromScreen(left, top, 0, 0, new System.Drawing.Size(width, height));
+                    return Imaging.CreateBitmapSourceFromHBitmap(
+                        screenBmp.GetHbitmap(),
+                        IntPtr.Zero,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+            }
+        }
+    }
+}
