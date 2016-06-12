e9ac943cb7bcffc0b99bcf7419641f847e6b8303
Rooni <Crispbaguette@gmail.com>
2016-06-12 21:13:26 +0200

    Changed the main window
    Haru is now a seperate window. Hooks are moved to the new main window.
    
    Also deleted a unused window.

--- Haru/FormHaru.xaml.cs
+++ Haru/FormHaru.xaml.cs
@@ -18,19 +18,15 @@ namespace Haru
 {
     public static class WindowsServices
     {
-        const int WS_EX_TRANSPARENT = 0x00000020;
-        const int GWL_EXSTYLE = (-20);
-
         [DllImport("user32.dll")]
         static extern int GetWindowLong(IntPtr hwnd, int index);
-
         [DllImport("user32.dll")]
         static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 
         public static void SetWindowExTransparent(IntPtr hwnd)
         {
-            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
-            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+            var extendedStyle = GetWindowLong(hwnd, (-20));
+            SetWindowLong(hwnd, (-20), extendedStyle | 0x00000020);
         }
     }
 
@@ -39,85 +35,16 @@ namespace Haru
         public FormHaru()
         {
             InitializeComponent();
-
-            Loaded += FormHaru_Loaded;
-        }
-
-        private void FormHaru_Loaded(object sender, RoutedEventArgs e)
-        {
-            //this.Width /= 2;
-            //this.Height /= 2;
-            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
-            this.Top = System.Windows.SystemParameters.PrimaryScreenHeight - this.Height;
         }
 
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
-            
             var hwnd = new WindowInteropHelper(this).Handle;
             WindowsServices.SetWindowExTransparent(hwnd);
-            var helper = new WindowInteropHelper(this);
-            _source = HwndSource.FromHwnd(helper.Handle);
-            _source.AddHook(HwndHook);
-            RegisterHotKey();
+            this.Left = System.Windows.SystemParameters.PrimaryScreenWidth - this.Width;
+            this.Top = System.Windows.SystemParameters.PrimaryScreenHeight - this.Height;
         }
 
-        [DllImport("User32.dll")]
-        private static extern bool RegisterHotKey(
-            [In] IntPtr hWnd,
-            [In] int id,
-            [In] uint fsModifiers,
-            [In] uint vk);
-
-        [DllImport("User32.dll")]
-        private static extern bool UnregisterHotKey(
-            [In] IntPtr hWnd,
-            [In] int id);
-
-        private HwndSource _source;
-        private const int HOTKEY_ID = 9001;
-        
-
-        protected override void OnClosed(EventArgs e)
-        {
-            _source.RemoveHook(HwndHook);
-            _source = null;
-            UnregisterHotKey();
-            base.OnClosed(e);
-        }
-
-        private void RegisterHotKey()
-        {
-            var helper = new WindowInteropHelper(this);
-            if (!RegisterHotKey(helper.Handle, HOTKEY_ID, 0x0006, 0x34))
-            {
-                // handle error
-                System.Windows.Forms.MessageBox.Show("Couldn't register CTRL+SHIFT+4");
-            }
-        }
-
-        private void UnregisterHotKey()
-        {
-            var helper = new WindowInteropHelper(this);
-            UnregisterHotKey(helper.Handle, HOTKEY_ID);
-        }
-
-        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
-        {
-            const int WM_HOTKEY = 0x0312;
-            switch (msg)
-            {
-                case WM_HOTKEY:
-                    switch (wParam.ToInt32())
-                    {
-                        case HOTKEY_ID:
-                            new Snipper().ShowDialog();
-                            break;
-                    }
-                    break;
-            }
-            return IntPtr.Zero;
-        }
     }
 }
---
+++ Haru/FormMain.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Haru
+{
+    public class Hotkeying
+    {
+        [DllImport("User32.dll")]
+        private static extern bool RegisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id,
+            [In] uint fsModifiers,
+            [In] uint vk);
+
+        [DllImport("User32.dll")]
+        private static extern bool UnregisterHotKey(
+            [In] IntPtr hWnd,
+            [In] int id);
+
+        private Window win;
+        private HwndSource _source;
+
+        public Hotkeying()
+        {
+            this.win = Application.Current.MainWindow;
+            var helper = new WindowInteropHelper(win);
+            _source = HwndSource.FromHwnd(helper.Handle);
+            _source.AddHook(HwndHook);
+            Register();
+        }
+
+        public void Register()
+        {
+            var helper = new WindowInteropHelper(win);
+            if (!RegisterHotKey(helper.Handle, 9001, 0x0006, 0x34))
+            {
+                // handle error
+                System.Windows.Forms.MessageBox.Show("Hotkey 9001 in use.");
+            }
+            if (!RegisterHotKey(helper.Handle, 9002, 0x0006, 0x35))
+            {
+                // handle error
+                System.Windows.Forms.MessageBox.Show("Hotkey 9002 in use.");
+            }
+            if (!RegisterHotKey(helper.Handle, 9003, 0x0006, 0x36))
+            {
+                // handle error
+                System.Windows.Forms.MessageBox.Show("Hotkey 9003 in use.");
+            }
+        }
+
+        public void Unregister()
+        {
+            var helper = new WindowInteropHelper(win);
+            UnregisterHotKey(helper.Handle, 9001);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case 9001:
+                            Snipper.Snip();
+                        break;
+
+                        case 9002:
+                            Window haru = Application.Current.Windows.OfType<Window>().Where(x => x.Name == "windowHaru").FirstOrDefault();
+                            if (haru==null)
+                            {
+                                new FormHaru().Show();
+                            }
+                            else
+                            {
+                                haru.Close();
+                            }
+                           break;
+
+                        case 9003:
+                            new FormDebug().Show();
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+    }
+
+    public partial class FormMain : Window
+    {
+
+        private Hotkeying hk;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+            //Append code
+            hk = new Hotkeying();
+            WindowInteropHelper wndHelper = new WindowInteropHelper(this);
+
+            int exStyle = (int)GetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE);
+
+            exStyle |= (int)ExtendedWindowStyles.WS_EX_TOOLWINDOW;
+            SetWindowLong(wndHelper.Handle, (int)GetWindowLongFields.GWL_EXSTYLE, (IntPtr)exStyle);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            hk.Unregister();
+            base.OnClosed(e);
+        }
+
+        [Flags]
+        public enum ExtendedWindowStyles
+        {
+            // ...
+            WS_EX_TOOLWINDOW = 0x00000080,
+            // ...
+        }
+
+        public enum GetWindowLongFields
+        {
+            // ...
+            GWL_EXSTYLE = (-20),
+            // ...
+        }
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowLong(IntPtr hWnd, int nIndex);
+
+        public static IntPtr SetWindowLong(IntPtr hWnd, int nIndex, IntPtr dwNewLong)
+        {
+            int error = 0;
+            IntPtr result = IntPtr.Zero;
+            // Win32 SetWindowLong doesn't clear error on success
+            SetLastError(0);
+
+            if (IntPtr.Size == 4)
+            {
+                // use SetWindowLong
+                Int32 tempResult = IntSetWindowLong(hWnd, nIndex, IntPtrToInt32(dwNewLong));
+                error = Marshal.GetLastWin32Error();
+                result = new IntPtr(tempResult);
+            }
+            else
+            {
+                // use SetWindowLongPtr
+                result = IntSetWindowLongPtr(hWnd, nIndex, dwNewLong);
+                error = Marshal.GetLastWin32Error();
+            }
+
+            if ((result == IntPtr.Zero) && (error != 0))
+            {
+                throw new System.ComponentModel.Win32Exception(error);
+            }
+
+            return result;
+        }
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr", SetLastError = true)]
+        private static extern IntPtr IntSetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+        [DllImport("user32.dll", EntryPoint = "SetWindowLong", SetLastError = true)]
+        private static extern Int32 IntSetWindowLong(IntPtr hWnd, int nIndex, Int32 dwNewLong);
+
+        private static int IntPtrToInt32(IntPtr intPtr)
+        {
+            return unchecked((int)intPtr.ToInt64());
+        }
+
+        [DllImport("kernel32.dll", EntryPoint = "SetLastError")]
+        public static extern void SetLastError(int dwErrorCode);
+
+    }
+}
